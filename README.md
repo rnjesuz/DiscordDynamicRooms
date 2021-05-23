5380aedac0eaf703d703ebfc2a9691e8b8308bac
Ricardo Jesus <11352735+rnjesuz@users.noreply.github.com>
2021-05-23 14:58:40 +0100

    Refactoring && removing room's parent category restrictions
    * Refactored configs to provide better structure.
    * Added a config flag to create channels with or without a parent category.
    * Refactored code to reduce the 'long method' antipattern. More methods, properly named for their functionality.  Separation of responsibilities.
    * Refactored logging. Reduced logging to prevent a bloated output with unnecessary info. Introduced more pertinent logs on creation/deletion of channels.
    Removed unused/unnecessary code.

--- README.md
+++ README.md
@@ -2,10 +2,11 @@
 A discord bot to dinamically create voice rooms
 
 ## Configs
-* BOT_TOKEN: Bot token of discord developer application. Required for execution, to login as the given application.
-* REFRESH_RATE: Interval between each cycle of creation/deletion of voice channnels. In miliseconds.
-* GUILD_ID: This bot is made to be single-server. This is the server id in which we wish to run the bot.
-* PARENT_CATEGORY_ID: The bot creates voice rooms under a specific category. This is the id of that category.
+* BOT_APITOKEN: Bot token of discord developer application. Required for execution, to login as the given application.
+* BOT_REFRESHRATE: Interval between each cycle of creation/deletion of voice channnels. In miliseconds.
+* BOT_GUILD_ID: This bot is made to be single-server. This is the server id in which we wish to run the bot.
+* CHANNELS_PARENTCATEGORY_USE: True or false. If the bot should create channels in a specific category. If *true*, the *channels.parent_category.id* property must also be set.
+* CHANNELS_PARENTCATEGORY_ID: The bot creates voice rooms under a specific category. This is the id of that category.
 
 ## Run
 1) Copy the .env.example file to a new .env file
--- src/dynamicrooms.js
+++ src/dynamicrooms.js
@@ -3,52 +3,74 @@ require('console-stamp')(console, '[HH:MM:ss.l]');
 
 const { Client } = require('discord.js');
 const client = new Client()
-const REFRESH_RATE = process.env.REFRESH_RATE
-const PARENT_CATEGORY_ID = process.env.PARENT_CATEGORY_ID
+const REFRESH_RATE = process.env.BOT_REFRESHRATE
+const GUILD_ID = process.env.BOT_GUILD_ID
+const USE_PARENT_CATEGORY = process.env.CHANNELS_PARENTCATEGORY_USE
+const PARENT_CATEGORY_ID = process.env.CHANNELS_PARENTCATEGORY_ID
 
-client.login(process.env.BOT_API_TOKEN);
+client.login(process.env.BOT_APITOKEN);
 
 client.on('ready', () => {
-  console.log('DiscordDynamicRoom bot turning on');
+  console.log('DiscordDynamicRooms bot turning on');
   setInterval(() => {
     client.guilds
-    .fetch(process.env.GUILD_ID, false, false)
-    .then(guild => {
-      let openVoiceChannels = guild.channels.cache.filter(
-        channel => channel.parent &&
-                    channel.parent.id === PARENT_CATEGORY_ID &&
-                    channel.type === 'voice');
-        console.log('openVoiceChannels: ' + openVoiceChannels.array());
-        
-        let occupiedVoiceChannels = openVoiceChannels.filter(channel => channel.members.size >= 1);
-        console.log('nonEmptyVoiceChannels: ' + occupiedVoiceChannels.array());
-          
-        let emptyVoiceChannels = openVoiceChannels.filter(channel => channel.members.size === 0);
-        console.log('emptyVoiceChannels: ' + emptyVoiceChannels.array());
-          
-        if(emptyVoiceChannels.size === 0) {
-          guild.channels
-          .create('🔁·', {type: 'voice', parent: PARENT_CATEGORY_ID})
-          .then(console.log)
-          .catch(err => console.log(channel.name));
-        }
+    .fetch(GUILD_ID, false, false)
+    .then(guild => {      
+      let existingGuildChannels = guild.channels.cache.filter(
+        channel => filterGuildChannelsOnParentCategory(channel)
+      );
+      let emptyVoiceChannels = existingGuildChannels.filter(
+        channel => channel.members.size === 0 && channel.type === 'voice'
+      );
+      
+      if(emptyVoiceChannels.size === 0) {
+        createChannel(guild);
+      }
 
-        if (emptyVoiceChannels.size > 1 && openVoiceChannels.size > 1) {
-          if (emptyVoiceChannels.last()) {
-              emptyVoiceChannels.last().delete();
-          }
-        }
-        console.log('---------------------------------')
+      if (emptyVoiceChannels.size > 1 && existingGuildChannels.size > 1) {
+        deleteChannel(emptyVoiceChannels);
+      }
     })
-    .catch(console.log)
+    .catch(console.error)
   }, REFRESH_RATE)
 })
+
+function filterGuildChannelsOnParentCategory(channel) {
+  if(USE_PARENT_CATEGORY == true) {
+    return channel.parent && channel.parent.id === PARENT_CATEGORY_ID && channel.type === 'voice';
+  } else {
+    return !channel.parent && channel.type === 'voice';
+  }
+}
+
+function createChannel(guild) {
+  if(USE_PARENT_CATEGORY == true && PARENT_CATEGORY_ID) {
+    console.log('Creating new voice channel under parent category ' + PARENT_CATEGORY_ID);
+    guild.channels
+    .create('🔁', {type: 'voice', parent: PARENT_CATEGORY_ID})
+    .then(console.log)
+    .catch(err => console.error(channel.name, err));
+  } else {
+    console.log('Creating new voice channel');
+    guild.channels
+    .create('🔁', {type: 'voice'})
+    .then(console.log)
+    .catch(err => console.error(channel.name, err));
+  }
+}
+
+function deleteChannel(emptyVoiceChannels) {
+  if (emptyVoiceChannels.last()) {
+    console.log('Deleting voice channel: ' + emptyVoiceChannels.last() + ';' + emptyVoiceChannels.last().name);
+    emptyVoiceChannels.last().delete();
+  }
+}
 
 function shutdown() {
     if (client) {
         client.destroy();
     }
-    console.log('Shutting down DiscordDynamicRoom bot');
+    console.log('Shutting down DiscordDynamicRooms bot');
     process.exit();
 }
 
